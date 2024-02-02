9a7c6ea87706d53d0a3464820ef44542b5b72308
venilsutariya <venilsutariya7@gmail.com>
2024-02-02 20:35:56 +0530

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,7 +97,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-                    <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className=" object-cover"/>
                 </div>
             </div>
         </div>
